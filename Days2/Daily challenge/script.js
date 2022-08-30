e5ad93e24edf02192a9926fb2a23f63292562440
ibrahimneya07 <111282420+ibrahimneya07@users.noreply.github.com>
2022-08-30 13:15:50 +0000

    Delete Days2 directory

--- Days2/Daily challenge/script.js
+++
@@ -1,8 +0,0 @@
-let sentence="The movie is not that bad, I like it";
-let wordNot=sentence.indexOf("not");
-let wordBad=sentence.indexOf("bad");
-if(wordNot>wordBad){
-    console.log("The movie is good, I like it");
-}else{
-    console.log(sentence);
-}
